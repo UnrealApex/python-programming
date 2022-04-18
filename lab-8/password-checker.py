d474e85c61bd3e514affc74e87802da7f3f6a152
Aaron <dojo1011234@gmail.com>
2022-04-18 15:59:04 -0700

    Fixed wrong method being used

--- lab-8/password-checker.py
+++ lab-8/password-checker.py
@@ -1,6 +1,10 @@
 def check_password(password):
+    digits = 0
+    for i in password:
+        if i.isdigit():
+            digits += 1
 
-    if (len(password) >= 8 and password.isalpha()):
+    if (len(password) >= 8 and password.isalnum() and digits >= 2):
         print("Your password is good")
 
     else:
@@ -8,4 +12,4 @@ def check_password(password):
 # program that checks passwordz
 
 
-check_password("pas322sword")
+check_password("1234password")
