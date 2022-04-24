4df056eea418c1a955aacd096b649d062afbbb01
Aaron <dojo1011234@gmail.com>
2022-04-24 16:05:03 -0700

    Smallest in list

---
+++ lab-9/smallested-in-list.py
@@ -0,0 +1,7 @@
+# program that gets the index of the smallest number in a list
+
+# list of numbers
+numbers = input(
+    "Enter a set of integers to find the distinct ones in the set ").strip().split(" ")
+# print the index of the smallest number in the list
+print(numbers.index(min(numbers)))
