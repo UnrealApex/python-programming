64d1078270aeecee3a9be00847602ee777951bad
Aaron <dojo1011234@gmail.com>
2022-04-24 15:21:49 -0700

    Added comments

--- lab-9/average-outlier.py
+++ lab-9/average-outlier.py
@@ -1,15 +1,19 @@
 # list of numbers
 numbers = input(
     "Enter a set a numbers to find the outliers in the group: ").strip().split(" ")
-# computer the average of the numbers in the list
 average = 0
+# numbers at average
 at_average = 0
+# numbers above average
 above_average = 0
+# numbers below average
 below_average = 0
+# computer the average of the numbers in the list
 for i in numbers:
     average += int(i)
 average = average / len(numbers)
 
+# compute how many numbers are at, above, and below the average
 for i in numbers:
   if int(i) > average:
     above_average += 1
@@ -18,6 +22,7 @@ for i in numbers:
   else:
     at_average += 1
 
+# print the results
 print(f"The average of the numbers in the set is {average}\n\
 {at_average} numbers are equal to the average\n\
 {above_average} numbers are above the average\n\
