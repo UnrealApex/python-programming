b372d75a6fcd62aca08b9749208e97674a9b1ee8
Aaron <dojo1011234@gmail.com>
2022-04-24 15:00:44 -0700

    Finished logic

--- lab-9/occurance-lister.py
+++ lab-9/occurance-lister.py
@@ -1,7 +1,10 @@
-def occuranceLister():
-  x = input("Enter integers between 1 and 100 with a space between each: ")
-  x = x.split(" ")
-  for i in x:
-    print(f"{i} occurs {x.count(i)} times")
-
-occuranceLister()
+numbers = input("Enter integers between 1 and 100 with a space between each: ").split(" ")
+y = set(numbers)
+if (all(int(i) > 1 and int(i) < 100 for i in y)):
+  for i in y:
+    if int(i) > 1:
+      print(f"{i} occurs {numbers.count(i)} times")
+    else:
+      print(f"{i} occurs {numbers.count(i)} time")
+else:
+  print("Try Again!\n Make sure you enter numbers between 1 and 100")
