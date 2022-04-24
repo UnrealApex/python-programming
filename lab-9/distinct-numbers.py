6c53212e0f150351a93002c3193d6a17c54dd651
Aaron <dojo1011234@gmail.com>
2022-04-24 16:17:10 -0700

    Added colon

--- lab-9/distinct-numbers.py
+++ lab-9/distinct-numbers.py
@@ -1,6 +1,6 @@
 # list of numbers
 numbers = input(
-    "Enter a set of integers to find the distinct ones in the set ").strip().split(" ")
+    "Enter a set of integers to find the distinct ones in the set: ").strip().split(" ")
 # convert each array item to the type integer
 numbers = map(int, numbers)
 # print the results
--- lab-9/smallested-in-list.py
+++ lab-9/smallested-in-list.py
@@ -2,6 +2,6 @@
 
 # list of numbers
 numbers = input(
-    "Enter a set of integers to find the distinct ones in the set ").strip().split(" ")
+    "Enter a set of integers to find the distinct ones in the set: ").strip().split(" ")
 # print the index of the smallest number in the list
 print(numbers.index(min(numbers)))
