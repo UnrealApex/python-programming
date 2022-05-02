daa6d5ae6da06d04c02c44213f59e6668d6d07f1
Aaron <dojo1011234@gmail.com>
2022-05-02 16:26:28 -0700

    Corrected variable name

--- lab-10/matrix-sum.py
+++ lab-10/matrix-sum.py
@@ -3,7 +3,7 @@
 import random
 matrix = [[random.randint(0, 1) for i in range(4)] for j in range(4)]
 print(matrix)
-rows = [[row[0] for row in matrix]]
+rows = [[column [0] for column in matrix]]
 rows.sort()
 print(f"Largest row index: {matrix.index(max(matrix))}")
-print(f"Largest column index: {rows[-1]})")
+print(f"Largest column index: {column[-1]})")
