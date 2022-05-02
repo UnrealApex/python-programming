a3a59041f76647e393e424c38e1e30c1d72bc6cc
Aaron <dojo1011234@gmail.com>
2022-05-02 14:32:19 -0700

    Added column index output

--- lab-10/matrix-sum.py
+++ lab-10/matrix-sum.py
@@ -1,4 +1,7 @@
 import random
 matrix = [[random.randint(0, 1) for i in range(4)] for j in range(4)]
 print(matrix)
+rows = [[row[0] for row in matrix]]
+rows.sort()
 print(f"Largest row index: {matrix.index(max(matrix))}")
+print(f"Largest column index: {rows[-1]})")
