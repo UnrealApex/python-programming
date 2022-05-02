32cc9da76ff175ebd037662ebb80b9651e89c7d3
Aaron <dojo1011234@gmail.com>
2022-05-01 19:50:41 -0700

    Basic input and output

---
+++ lab-10/4x4-matrix.py
@@ -0,0 +1,7 @@
+row_1 = input("Enter 3-by-4 matrix row for row 1: ").split(" ")
+row_2 = input("Enter 3-by-4 matrix row for row 2: ").split(" ")
+row_3 = input("Enter 3-by-4 matrix row for row 3: ").split(" ")
+
+print(f"The sum of the elements for column 1 is {sum(row_1)}")
+print(f"The sum of the elements for column 2 is {sum(row_2)}")
+print(f"The sum of the elements for column 3 is {sum(row_3)}")
