84dd596cfdee03936b6f6d347742e819d3f8ab73
Aaron <dojo1011234@gmail.com>
2022-05-02 15:35:37 -0700

    Met assignment requirements

--- lab-10/matrix-column-sum.py
+++ lab-10/matrix-column-sum.py
@@ -1,8 +1,11 @@
-# program that prints out the sum of all the numbers in the column in a given matrix
+# program that prints out the sum of all the numbers in the column in a given matrix for a 3x4 matrix
 def sumColumn(matrix, column):
     sum = 0
     for i in range(len(matrix)):
         sum += matrix[i][column]
     print(f"Sum of the elements for column {column} is {sum}")
 
-sumColumn([[1.5, 2, 3, 4], [5.5, 6, 7, 8], [9.5, 1, 3, 1]], 1)
+
+for i in range(4):
+    sumColumn([[1.5, 2, 3, 4], [5.5, 6, 7, 8], [9.5, 1, 3, 1]], i)
+
