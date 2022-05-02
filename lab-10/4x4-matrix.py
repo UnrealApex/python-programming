0071ff1f36f462e144449139fa34cb910ddc174e
Aaron <dojo1011234@gmail.com>
2022-05-01 20:07:14 -0700

    4x4-matrix.py

--- lab-10/4x4-matrix.py
+++ lab-10/4x4-matrix.py
@@ -1,7 +1,15 @@
-row_1 = input("Enter 3-by-4 matrix row for row 1: ").split(" ")
-row_2 = input("Enter 3-by-4 matrix row for row 2: ").split(" ")
-row_3 = input("Enter 3-by-4 matrix row for row 3: ").split(" ")
+# inputs
+row_1 = input("Enter 3-by-4 matrix row for row 1: ").strip().split(" ")
+row_2 = input("Enter 3-by-4 matrix row for row 2: ").strip().split(" ")
+row_3 = input("Enter 3-by-4 matrix row for row 3: ").strip().split(" ")
 
+# convert each value in the matrix rows to a float
+row_1 = map(float, row_1) 
+row_2 = map(float, row_2) 
+row_3 = map(float, row_3) 
+
+# print the result
+print()
 print(f"The sum of the elements for column 1 is {sum(row_1)}")
 print(f"The sum of the elements for column 2 is {sum(row_2)}")
 print(f"The sum of the elements for column 3 is {sum(row_3)}")
