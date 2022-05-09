c6b961d2ddcf4f6554776c6bf22d11d340e94840
Aaron <dojo1011234@gmail.com>
2022-05-09 00:05:07 -0700

    Added comment on formula used to find triangle area

--- lab-11/geometric-object.py
+++ lab-11/geometric-object.py
@@ -12,6 +12,7 @@ class GeometricObject:
             def getArea(self):
                 """return the area of a given Triangle instance"""
                 # return 0.5 * (self.base * self.height)
+                # get triangle area using Heron's formula
                 semi-perimeter = 0.5 * (self.side1 + self.side2 + self.side3)
                 return math.sqrt(semi-perimeter * (semi-perimeter - side1) * (semi-perimeter - side2) * (semi-perimeter- side3))
 
