8e53f212e1468c8a0f1cd8ec56685a42f0308c4f
UnrealApex <dojo1011234@gmail.com>
2022-05-10 00:26:21 -0700

    Monthly interest rate logic

--- lab-11/account.py
+++ lab-11/account.py
@@ -23,7 +23,7 @@ class Account:
         self.__annual_interest_rate = new_annual_interest_rate
 
     def get_monthly_interest(self):
-        pass
+        return self.__balance * (annual_interest_rate / 0.12)
     
     def get_monthly_interest_rate(self):
         pass
