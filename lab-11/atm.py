fc736e4990a9ddaff2e771ab3bb2048b145d1cf5
UnrealApex <dojo1011234@gmail.com>
2022-05-10 01:28:07 -0700

    Fixed values being pass for account instances

--- lab-11/atm.py
+++ lab-11/atm.py
@@ -1,15 +1,14 @@
 from account import Account
 
-account_1 = Account(i + 1, 100)
-account_2 = Account(i + 1, 100)
-account_3 = Account(i + 1, 100)
-account_3 = Account(i + 1, 100)
-account_4 = Account(i + 1, 100)
-account_5 = Account(i + 1, 100)
-account_6 = Account(i + 1, 100)
-account_7 = Account(i + 1, 100)
-account_8 = Account(i + 1, 100)
-account_9 = Account(i + 1, 100)
+account_1 = Account(1 + 2, 100)
+account_2 = Account(2 + 1, 100)
+account_3 = Account(3 + 1, 100)
+account_4 = Account(4 + 1, 100)
+account_5 = Account(5 + 1, 100)
+account_6 = Account(6 + 1, 100)
+account_7 = Account(7 + 1, 100)
+account_8 = Account(8 + 1, 100)
+account_9 = Account(9 + 1, 100)
 
 def get_id():
     global id
@@ -27,3 +26,6 @@ def main_menu(id):
         3: deposit\
         4: exit\
           ")
+    selection = int(input("Enter your selection here: "))
+    if (selection == 1):
+        eval(f"account_{id}.get_balance()")
