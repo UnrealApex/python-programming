e40044b97f91f19736d6af080748e3ee2cd844c6
Aaron <79121360+UnrealApex@users.noreply.github.com>
2022-05-09 21:43:00 -0700

    Renamed invalid variable

--- lab-11/triangle.py
+++ lab-11/triangle.py
@@ -12,8 +12,8 @@ class Triange(GeometricObject):
         """return the area of a given Triangle instance"""
         # return 0.5 * (self.base * self.height)
         # get triangle area using Heron's formula
-        semi-perimeter = 0.5 * (self.side1 + self.side2 + self.side3)
-        return math.sqrt(semi-perimeter * (semi-perimeter - side1) * (semi-perimeter - side2) * (semi-perimeter- side3))
+        semi_perimeter = 0.5 * (self.side1 + self.side2 + self.side3)
+        return math.sqrt(semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter- side3))
 
     def getPerimeter(self):
         return self.side1 + self.side2 + self.side3
