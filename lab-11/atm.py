8ebe853effb35a1a8d080602befa2d86b857f560
UnrealApex <dojo1011234@gmail.com>
2022-05-10 01:22:37 -0700

    Main menu

--- lab-11/atm.py
+++ lab-11/atm.py
@@ -1,6 +1,5 @@
 from account import Account
 
-
 account_1 = Account(i + 1, 100)
 account_2 = Account(i + 1, 100)
 account_3 = Account(i + 1, 100)
@@ -15,13 +14,13 @@ account_9 = Account(i + 1, 100)
 def get_id():
     global id
     id = prompt("Enter an account id")
-        if (isinstance(id, str)):
-            main_menu()
+        if (isinstance(id, str) and 0 < id < 10):
+            main_menu(id)
         else:
             print("Invalid id, try again!")
             get_id()
 
-def main_menu()
+def main_menu(id)
     print("Main Menu\
         1: check balance
         2: withdraw
@@ -29,4 +28,5 @@ def main_menu()
         4: exit
           ")
     selection = int(input("Enter your selection here: "))
-    if (selection
+    if (selection == 1):
+        eval(f"account_{id}.get_balance()")
