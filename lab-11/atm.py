717de73a4350c51e175056f1a4e7ec1642f2c20b
UnrealApex <dojo1011234@gmail.com>
2022-05-10 01:19:30 -0700

    account instances

---
+++ lab-11/atm.py
@@ -0,0 +1,32 @@
+from account import Account
+
+
+account_1 = Account(i + 1, 100)
+account_2 = Account(i + 1, 100)
+account_3 = Account(i + 1, 100)
+account_3 = Account(i + 1, 100)
+account_4 = Account(i + 1, 100)
+account_5 = Account(i + 1, 100)
+account_6 = Account(i + 1, 100)
+account_7 = Account(i + 1, 100)
+account_8 = Account(i + 1, 100)
+account_9 = Account(i + 1, 100)
+
+def get_id():
+    global id
+    id = prompt("Enter an account id")
+        if (isinstance(id, str)):
+            main_menu()
+        else:
+            print("Invalid id, try again!")
+            get_id()
+
+def main_menu()
+    print("Main Menu\
+        1: check balance
+        2: withdraw
+        3: deposit
+        4: exit
+          ")
+    selection = int(input("Enter your selection here: "))
+    if (selection
