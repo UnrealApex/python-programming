2c5404970cf72f2fec67428e364c51f17f50420e
UnrealApex <dojo1011234@gmail.com>
2022-05-12 20:38:21 -0700

    Added comments

--- lab-11/stack.py
+++ lab-11/stack.py
@@ -5,10 +5,12 @@ class Stack(list):
 
 
   def getStack(self):
+    """Prompts the user to enter five strings and stores them in a stack."""
     for i in range(5):
       self.stack.append(input(f"Enter string {i + 1} out of 5: "))
 
   def reverseStack(self):
+    """Reverses the order of the stack and displays it."""
     for i in range(1, 6):
       print(self.stack[-i])
 
