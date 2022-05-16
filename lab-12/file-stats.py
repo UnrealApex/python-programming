f90215534a0b652039fe27298e208213f35f8cdf
Aaron <dojo1011234@gmail.com>
2022-05-16 14:19:53 -0700

    file characters and lines

---
+++ lab-12/file-stats.py
@@ -0,0 +1,8 @@
+import os
+def stats(file_path):
+    file = open(file_path, "r")
+    file_data = file.read()
+    file_characters = len(file_data)
+    file_lines = int(sum(1 for line in file_data) / 10)
+
+stats(os.path.expanduser('~') + "\\src\\python-programming\\lab-12\\test.txt")
