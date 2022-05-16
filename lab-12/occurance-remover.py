b28445fc5464cd66548a24e595fa5015612b8609
Aaron <dojo1011234@gmail.com>
2022-05-16 02:30:33 -0700

    fixed occurances not removing

--- lab-12/occurance-remover.py
+++ lab-12/occurance-remover.py
@@ -1,12 +1,17 @@
-# TODO: make sure that program does not overwrite file contents
 import os
 def occurance_remover(file_path, removal_string):
-    target_file = open(file_path, "r+")
-    occurances = target_file.read().count(removal_string)
-    if removal_string in target_file.read():
-       removal_string = removal_string.replace(removal_string, "")
-    target_file.close()
-    print(f"Removed {occurances} of {removal_string} from {os.path.basename(file_path)}")
+    occurances = open(file_path, "r").read().count(removal_string)
+    # Read in the file
+    with open(file_path, "r") as file:
+      filedata = file.read()
 
+    # Remove the string given from the file
+    filedata = filedata.replace(removal_string, "")
+
+    # Write the contents to the file without the removal strings
+    with open(file_path, 'w') as file:
+      file.write(filedata)
+    file.close()
+    print(f"Removed {occurances} occurances of {removal_string} from {os.path.basename(file_path)}")
     
 occurance_remover(r"C:\Users\s158658\src\python-programming\lab-12\test.txt", "foo")
