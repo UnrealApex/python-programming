3ab2cf9474820602fa7b89f928544d333de0bf63
Aaron <dojo1011234@gmail.com>
2022-05-16 14:54:37 -0700

    file stats

--- lab-12/file-stats.py
+++ lab-12/file-stats.py
@@ -1,9 +1,14 @@
 import os
+
 def stats(file_path):
     file = open(file_path, "r")
     file_data = file.read()
     file_characters = len(file_data)
     file_lines = int(sum(1 for line in file_data) / 10)
     file_words = len(file_data.split())
+    print(f"File statistics for {os.path.basename(file_path)}: \n\
+{file_characters} characters\n\
+{file_words} words\n\
+{file_lines} lines")
 
 stats(os.path.expanduser('~') + "\\src\\python-programming\\lab-12\\test.txt")
